f5c039f0d7e9b50b36360a43f9ba57a8427adcab
emanuelnav <emanuelnavarro.dev@gmail.com>
2022-04-27 16:14:13 -0300

    Updates

---
+++ precache-manifest.a31e492c9ea58055eb76ea2be355d6c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c87b258fb380a32a4fb",
+    "url": "/todo-react-app/static/css/main.a9e89dd0.chunk.css"
+  },
+  {
+    "revision": "6c87b258fb380a32a4fb",
+    "url": "/todo-react-app/static/js/main.6c87b258.chunk.js"
+  },
+  {
+    "revision": "7b188507b63bf58db150",
+    "url": "/todo-react-app/static/css/1.73693eaa.chunk.css"
+  },
+  {
+    "revision": "7b188507b63bf58db150",
+    "url": "/todo-react-app/static/js/1.7b188507.chunk.js"
+  },
+  {
+    "revision": "38d8e3f4f01c8802a632",
+    "url": "/todo-react-app/static/js/runtime~main.38d8e3f4.js"
+  },
+  {
+    "revision": "1ed478a6b265d4b4f5c26bb063203588",
+    "url": "/todo-react-app/static/media/bootstrap-icons.1ed478a6.woff2"
+  },
+  {
+    "revision": "e4cc83ddcec34db65724eb56c5230b4c",
+    "url": "/todo-react-app/static/media/bootstrap-icons.e4cc83dd.woff"
+  },
+  {
+    "revision": "71553f01fa0e369c7288f53498b832f2",
+    "url": "/todo-react-app/index.html"
+  }
+];
